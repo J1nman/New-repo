ccbd27647540fff3092c98b22da31611a13367d5
PeterStrom <peterstromprivat@gmail.com>
2020-05-20 20:22:44 +0200

    uppg

--- netflix_read_data.py
+++ netflix_read_data.py
@@ -4,3 +4,12 @@ import pandas as pd
 df_director = pd.read_csv("data/netflix1.csv")
 df_movies = pd.read_csv("data/netflix2.csv")
 
+# Kolla antal rader i båda DF
+
+
+# Kolla antal unika regisorer i varje DF
+
+
+# Skapa en ny DF med enbart alla regisorer i df_director, men med alla filmer de har gjort (dvs
+# flera rader per regisor). Tips: kolla att antal unika regisörer är samma som i df_director.
+
