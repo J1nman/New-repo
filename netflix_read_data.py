06582f9023e10fec14d85247ea64e50436140b8d
PeterStrom <peterstromprivat@gmail.com>
2020-05-21 21:36:25 +0200

    ...

--- netflix_read_data.py
+++ netflix_read_data.py
@@ -26,6 +26,9 @@ df_a = pd.merge(df_director, df_movie, on='director')
 df_a = df_a[df_a['type'] != 'TV Show']
 
 
+# NOTE: Fel. Alla regisörer är inte med från df_director. Kolla ALLTID att koden du kör ger de
+# resultat du förväntar dig. Jag gör alltid det också (efter 7 år) annars blir det ofta fel.
+# len(df_a.director.unique())
 
 
 # Skapa en motsvarande DF med alla regisörer i df_movies som börjar på bokstaven A i förnamn. Kalla den df_b.
@@ -34,6 +37,8 @@ df_b = df_movie[df_movie['director'] == 'A&']
 df_movie = df_movie.dropna()
 df_b = df_movie[df_movie.director.str.startswith('A')]
 
+# Fel. Den ska vara motsvarande, dvs alla kolumner som finns i df_a (inkl age).
+
 # Skapa en tredje DF (df_c) med alla regisörer som är med i både df_a och df_b.
 
 df = [df_a, df_b]
@@ -41,9 +46,13 @@ df_c = pd.concat(df)
 df_c = df_c[['director']]
 df_c = df_c.drop_duplicates()
 
+# Ja, det här stämmer. bra lösning. dock tänkte jag att alla kolumnerna skulle vara kvar - var otydlig.
+
 # Hur många regisörer är med i df_b men inte i df_c?
 
 df_b = df_b[['director']]
 df_b = df_b.drop_duplicates()
 
 pd.concat([df_b, df_c]).drop_duplicates(keep=False)
+
+# Det här stämmer nog inte? Eller? Tips: använd merge istället för concat.
