130772befa2235bfa716fc9a8ca8a3f654901dc9
Tony Ström <strom91@hotmail.se>
2020-05-22 12:39:17 +0200

    uppdated

--- netflix_read_data.py
+++ netflix_read_data.py
@@ -22,17 +22,25 @@ df_movie['director'].nunique()
 # alla kolumner i båda df. Kalla den nya för df_a.
 
 
-df_a = pd.merge(df_director, df_movie, on='director')
+df_a = pd.merge(df_director, df_movie, how='left', on='director')
 df_a = df_a[df_a['type'] != 'TV Show']
-
 
 
 
 # Skapa en motsvarande DF med alla regisörer i df_movies som börjar på bokstaven A i förnamn. Kalla den df_b.
 
-df_b = df_movie[df_movie['director'] == 'A&']
+
 df_movie = df_movie.dropna()
-df_b = df_movie[df_movie.director.str.startswith('A')]
+df_g = df_movie[df_movie.director.str.startswith('A')]
+df_movie = df_movie[['director']]
+df_b = pd.merge(df_movie, df_a, how='right', on='director')
+
+df_b = df_b.dropna()
+df_b = df_b[df_b.director.str.startswith('A')]
+df_b = pd.concat([df_b, df_g])
+df_b = df_b.drop_duplicates()
+
+len(df_b.director.unique()) == len(df_g.director.unique())
 
 # Skapa en tredje DF (df_c) med alla regisörer som är med i både df_a och df_b.
 
@@ -42,8 +50,10 @@ df_c = df_c[['director']]
 df_c = df_c.drop_duplicates()
 
 # Hur många regisörer är med i df_b men inte i df_c?
-
+# tvärtom gissar jag på
 df_b = df_b[['director']]
 df_b = df_b.drop_duplicates()
 
+
 pd.concat([df_b, df_c]).drop_duplicates(keep=False)
+print(2732 + 241)
