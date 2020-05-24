10fba49eb47a0a08b3369a11eb75ca39b1f938bf
Peter Ström <peterstromprivat@gmail.com>
2020-05-24 15:48:00 +0200

    uppd

--- netflix_read_data.py
+++ netflix_read_data.py
@@ -30,21 +30,11 @@ df_a = df_a[df_a['type'] != 'TV Show']
 # Skapa en motsvarande DF med alla regisörer i df_movies som börjar på bokstaven A i förnamn. Kalla den df_b.
 
 
-df_movie = df_movie.dropna()
-df_g = df_movie[df_movie.director.str.startswith('A')]
-df_movie = df_movie[['director']]
-df_b = pd.merge(df_movie, df_a, how='right', on='director')
+df1 = df_movie.dropna()
+df1 = df1[df1.director.str.startswith('A')]
+df_b = pd.merge(df_director, df1, how='right', on='director')
+df_b = df_b[df_b['type'] != 'TV Show']
 
-df_b = df_b.dropna()
-df_b = df_b[df_b.director.str.startswith('A')]
-df_b = pd.concat([df_b, df_g])
-df_b = df_b.drop_duplicates()
-
-len(df_b.director.unique()) == len(df_g.director.unique())
-
-# Ser ut att funka. Men kan kanske göras snabbare med bara två rader:
-df_g = df_movie[df_movie.director.str.startswith('A')]
-df_b = pd.merge(df_director, df_g, how='right', on='director')
 
 # Skapa en tredje DF (df_c) med alla regisörer som är med i både df_a och df_b.
 
@@ -54,20 +44,57 @@ df_c = df_c[['director']]
 df_c = df_c.drop_duplicates()
 
 # Rätt, men om man vill ha med alla kolumnerna så kan man göra:
-df_c = pd.merge(df_director, df_movies, how='outer', on='director')
+df2 = pd.merge(df_a, df_b, how='outer', on='director')
 
-# Hur många regisörer är med i df_b men inte i df_c?
-# tvärtom gissar jag på
-df_b = df_b[['director']]
-df_b = df_b.drop_duplicates()
+len(df_c.director.unique()) == len(df2.director.unique())
+# Hur många regisörer är med i df_c men inte i df_b?
 
+df_d = df_b[['director']]
+df_d = df_d.drop_duplicates()
+df_d = pd.concat([df_d, df_c]).drop_duplicates(keep=False)
 
-pd.concat([df_b, df_c]).drop_duplicates(keep=False)
-print(2732 + 241)
 
 # Funkar! :)
 
 # Med df_a, behåll enbart den film som har högst rating per regisör.
 
+df_e = df_a.dropna()
+df_e['duration'] = df_e['duration'].str.replace(' min', '')
+df_e['duration'] = pd.to_numeric(df_e.duration, errors='coerce')
+
+df_e = df_e.loc[df_e.groupby('director')['duration'].idxmax()].sort_values('duration', ascending=False)
+df_e = df_e.sort_values(by=['director'])
+
+
+
 
 # Med df_a, skapa en ny DF som har en kolumn medelvärde av rating per regisör (dvs en rad per regisör).
+
+df_f = df_a
+df_f = df_f[df_f['type'] != 'TV Show']
+
+df_f = df_f.dropna()
+df_f['duration'] = df_f['duration'].str.replace(' min', '')
+df_f['duration'] = pd.to_numeric(df_f.duration, errors='coerce')
+
+df_f = df_f.groupby('director', as_index=False)['duration'].mean()
+df_f = df_f.sort_values(by=['director'])
+
+
+len(df_f.director.unique()) == len(df_e.director.unique())
+
+# hur många filmer/Tv Shows har varje regissör gjort
+df_g = df_movie
+df_g = df_g.groupby(['director']).size().reset_index(name='counts')
+df_g = df_g.sort_values(by=['counts'], ascending=False)
+
+
+# hur många regissörer har gjort hur många filmer/Tv Shows
+
+df_g.counts.value_counts()
+
+# alla filmer från de regisörer som gjort exakt 2 filmer
+df_h = df_movie[['director', 'title']]
+counts = df_h['director'].value_counts()
+df_h = df_h[df_h['director'].isin(counts.index[counts == 2])]
+df_h = df_h.sort_values(by=['director'], ascending=True)
